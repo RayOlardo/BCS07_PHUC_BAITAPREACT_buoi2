a828babc3c08650713d00f1cbbd0bfe2003eb4fe
RayOlardo <phuclam1044@gmail.com>
2023-06-22 23:36:16 +0700

    first commit

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Ex_Glasses from './Components/Ex_Glasses/Ex_Glasses';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Ex_Glasses />
+);
+
+
